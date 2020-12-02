99b40a23fc6b4c7f6131495428f12d20f18455b4
MARTINEZ, MAURICIO <mauricio.martinez@kroger.com>
2020-12-02 14:10:32 -0600

    Adding action

--- src/main.sh
+++ src/main.sh
@@ -2,6 +2,7 @@
 function main {
   # Source the other files to gain access to their functions
   secrets=${INPUT_SECRETS}
+  echo $secrets
   jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' <<< "$secrets" >> $GITHUB_ENV
   echo $GITHUB_ENV
 }
